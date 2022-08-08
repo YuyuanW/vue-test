a2366eee42101d11ec76fede433dab8da6db0aaa
yuyuan <1413733614@qq.com>
2022-08-08 21:20:16 +0800

    配置svg-spirite-loader的vue.config设置

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="nav">
-      <router-link to="/tags">标签</router-link>
+      <router-link to="/tags">标签<svg><use x-href="x"></use></svg></router-link>
       <router-link to="/change">记账</router-link>
       <router-link to="/money">统计</router-link>
     </div>
@@ -8,9 +8,14 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import chart from '@/assets/icons/chart.svg'
+
+console.log(chart)
 export default Vue.extend({
     name : 'Nav'
+    
 })
+
 </script>
 
 <style lang="scss" scoped>
