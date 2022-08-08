f9036d58a8f979bcdeaf7f24afa36624e66574ed
yuyuan <1413733614@qq.com>
2022-08-08 21:27:40 +0800

    引入三个图标成功

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="nav">
-      <router-link to="/tags">标签<svg><use xlink:href="#chart"></use></svg></router-link>
-      <router-link to="/change">记账</router-link>
-      <router-link to="/money">统计</router-link>
+      <router-link to="/tags"><svg><use xlink:href="#tags" /></svg>标签</router-link>
+      <router-link to="/change"><svg><use xlink:href="#money" /></svg>记账</router-link>
+      <router-link to="/money"><svg><use xlink:href="#chart" /></svg>统计</router-link>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import chart from '@/assets/icons/chart.svg'
-
-console.log(chart)
+import tags from '@/assets/icons/tags.svg'
+import money from '@/assets/icons/money.svg'
+console.log(chart,tags,money)
 export default Vue.extend({
     name : 'Nav'
     
