a4b02e4105b5d1535e44eb6bd239a1211274d923
yuyuan <1413733614@qq.com>
2022-08-08 22:15:48 +0800

    底部导航的flex布局和border-box的css样式编写

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="nav">
-      <router-link to="/tags"><Icon iconName="tags" />标签</router-link>
-      <router-link to="/change"><Icon iconName='money'/>记账</router-link>
-      <router-link to="/money"><Icon iconName='chart'/>统计</router-link>
+      <router-link to="/tags" class="routerItems"><Icon iconName="tags" /><p>标签</p></router-link>
+      <router-link to="/change" class="routerItems"><Icon iconName='money'/><p>记账</p></router-link>
+      <router-link to="/money" class="routerItems"><Icon iconName='chart'/><p>统计</p></router-link>
     </div>
 </template>
 
@@ -18,4 +18,45 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+  .nav{
+    box-shadow: 0px 0px 2px rgb(0,0,0,0.25);
+
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    align-items: center;
+    justify-content: space-around;
+    >a{
+      text-decoration: none;
+    }
+    > .routerItems{
+      flex-grow:1;
+
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      >svg{
+        
+        width:30px;
+        height: 30px;
+        padding-top:4px;
+
+        fill:#999999 ;
+      }
+      >p{
+        font-family: 'Source Han Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 12px;
+        color: #BFBFBF;
+
+        padding :2px 0px 6px 0px;
+        margin:0px;
+      }
+
+    }
+  }
 </style>
