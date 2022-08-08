6a1d23681060c47f9a8856c56dd5437817d6f604
yuyuan <1413733614@qq.com>
2022-08-08 21:44:15 +0800

    封装icons一次读取多个svg文件

---
+++ src/components/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg><use v-bind:xlink:href="'#'+iconName" /></svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
+export default Vue.extend({
+    name:'Icon',
+    props:['iconName']
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
